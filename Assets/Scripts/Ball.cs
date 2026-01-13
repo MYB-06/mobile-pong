a5214d7ba5848cb04c3558a51abd21d9df8ebcfb
MYB-06 <06myb06@gmail.com>
2026-01-13 22:59:11 +0300

    fix: prevent horizontal ball movement

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -40,6 +40,23 @@ namespace PongGame.Gameplay
         private void OnCollisionEnter2D(Collision2D collision)
         {
             _currentSpeed += speedIncrease;   
+
+            PreventDeadlock();
+        }
+
+        private void PreventDeadlock()
+        {
+            Vector2 velocity = _rigidbody2D.linearVelocity;
+            float minAngle = 30f;
+
+            if(Mathf.Abs(velocity.y) < _currentSpeed * Mathf.Sin(minAngle * Mathf.Deg2Rad))
+            {
+                float sign = velocity.y >= 0 ? 1f : -1f;
+                velocity.y = sign * _currentSpeed * Mathf.Sin(minAngle * Mathf.Deg2Rad);
+                velocity.x = Mathf.Sign(velocity.x) * _currentSpeed * Mathf.Cos(minAngle * Mathf.Deg2Rad);
+
+                _rigidbody2D.linearVelocity = velocity;
+            }
         }
     }
 }
