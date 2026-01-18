04e34f8241f73011fae200c452de99591c3c93e7
MYB-06 <06myb06@gmail.com>
2026-01-18 16:21:43 +0300

    add: paddle bounce logic

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,3 +1,4 @@
+using System;
 using PongGame.Input;
 using UnityEngine;
 
@@ -11,6 +12,8 @@ namespace PongGame.Gameplay
         [Header("Boundaries")]
         [SerializeField] private float minX = -2f;
         [SerializeField] private float maxX = 2f;
+        [Header("Bounce Settings")]
+        [SerializeField] private float maxBounceAngle;
         private Rigidbody2D _rigidbody2D;
         private IInputProvider _inputProvider;
 
@@ -42,6 +45,34 @@ namespace PongGame.Gameplay
             Vector3 pos = transform.position;
             pos.x = Mathf.Clamp(pos.x, minX, maxX);
             transform.position = pos;
+        }
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.TryGetComponent<Ball>(out Ball ball))
+            {
+                HandleBallBounce(collision, ball);
+            }
+        }
+
+        private void HandleBallBounce(Collision2D collision, Ball ball)
+        {
+            ContactPoint2D contact = collision.GetContact(0);
+    
+            float paddleHeight = GetComponent<BoxCollider2D>().bounds.size.y;
+            float hitOffset = contact.point.y - transform.position.y;
+    
+            float normalizedHit = hitOffset / (paddleHeight / 2f);
+            normalizedHit = Mathf.Clamp(normalizedHit, -1f, 1f);
+    
+            float bounceAngle = normalizedHit * maxBounceAngle;
+            float angleInRadians = bounceAngle * Mathf.Deg2Rad;
+            Vector2 direction = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians));
+    
+            direction.x = contact.normal.x > 0 ? Mathf.Abs(direction.x) : -Mathf.Abs(direction.x);
+    
+            Rigidbody2D ballRb = ball.GetComponent<Rigidbody2D>();
+            float ballSpeed = ballRb.linearVelocity.magnitude;
+            ballRb.linearVelocity = direction.normalized * ballSpeed;
         }
     }
 }
